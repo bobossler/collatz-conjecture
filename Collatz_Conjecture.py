65f84e5642b520fd7d6c168640d38d5652839081
Bob Ossler <bob.ossler@gmail.com>
2020-05-28 23:05:56 -0500

    Added user prompt

--- Collatz_Conjecture.py
+++ Collatz_Conjecture.py
@@ -1,16 +1,22 @@
-#Set starting number
-num = 28
+#Get starting number from user
+num = -1
+while num <= 0:
+	user_response = input ("Enter a positive integer to start with? ")
+	num = int(user_response)
 
 #Initialize iterator
 i = 0
 
+#Print the starting number
+print ("Starting number =",num)
+
 #Start main loop
 while num != 1:
-    print (num)
+    i += 1
     if num % 2 == 0:
         num /= 2
     else:
         num = num * 3 + 1
-    i += 1
+    print (num)
     
 print ("It took", i, "steps to reach", num)
